bfada492a74296e52c8045912457b51d0c2ac5fb
Kasperi Tervonen <kasperi.tervonen@gmail.com>
2022-08-28 23:34:35 +0300

    Add Rook class

---
+++ src/Rook.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class Rook extends Piece {
+
+    private final Piece.Color Color;
+
+    public Rook(Point p, Color c) {
+        this.location = p;
+        this.Color = c;
+    }
+}
